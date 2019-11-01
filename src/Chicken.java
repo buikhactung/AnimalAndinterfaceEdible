5ecb061d70a63837bd6d9234a78664c98fe8387e
Your Name <you@example.com>
2019-11-01 15:30:03 +0700

    nopbaithuchanh

---
+++ src/Chicken.java
@@ -0,0 +1,8 @@
+public  class Chicken extends Animal implements Edible{
+    public String makeSound() {
+        return "Chicken: chuck-chuck";
+    }
+    public String howToEat() {
+        return "could be fried";
+    }
+}
